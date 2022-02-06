052b061bdb6f72197ed3faffc8dc762c8a71f9d0
sanjaikumar <sanjailucifer666@gamil.com>
2022-02-06 15:52:41 +0530

    adding features

---
+++ project/script.js
@@ -0,0 +1,16 @@
+// const songfetcher = async function () {
+//   const url = await fetch("https://musicbrainz.org/ws/2/amarica/recording?query=artist:%22The%20Beatles%22&limit=1");
+// //   const data = await url.json();
+//   console.log(url);
+// };
+
+// songfetcher();
+
+// // ("http://127.0.0.1:5000/result/?query=https://www.jiosaavn.com/album/mudhal-nee-mudivum-nee-title-track-from-mudhal-nee-mudivum-nee/KBfF1NaeBEk_");
+// // fetch("https://musicbrainz.org/ws/2/");
+// //   .then(function (response) {
+// //     return response.json();
+// //   })
+// //   .then(function (data) {
+// //     console.log(data);
+// //   });
